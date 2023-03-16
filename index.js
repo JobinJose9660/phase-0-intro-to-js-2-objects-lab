8e669f538fd13d6210fa73910d74eb3ecf038776
Jobin Jose <jjobin9660@gmail.com>
2023-03-16 15:19:28 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name : "Jobin",
+    StreetAddress : "189 Littleton"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newEmployee = {...obj}
+    newEmployee[key]=value;
+    return newEmployee
+}
+const employees = updateEmployeeWithKeyAndValue(employee,"sam","'11 Broadway'");
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value;
+    return obj;
+}
+const employeess = destructivelyUpdateEmployeeWithKeyAndValue(employee,"sam","12 Broadway");
+
+function deleteFromEmployeeByKey(obj,key,value){
+    const newEmployee = {...obj}
+    newEmployee[key]=value;
+    return newEmployee
+}
+const employeesss  = deleteFromEmployeeByKey(employee);
+
+function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+    return obj;
+}
